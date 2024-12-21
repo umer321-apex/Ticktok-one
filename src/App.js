e9d0733e0d375a64422809fc2bd5955d92173a1e
umer321-apex <mu0829332@gmail.com>
2024-12-21 21:24:55 +0500

    frontend commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
 
-function App() {
+import Navbar from "./components/Navbar";
+import Login from "./components/Login";
+import Register from "./components/Register";
+
+import UploadPage from './components/videoUpload'
+import FeedPage from './components/videoList'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+    
+        <Router>
+          <Navbar />
+          <Routes>
+            {/* <Route path="/" element={<HomePage />} /> */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/upload" element={<UploadPage/>} />
+            <Route path="/" element={<FeedPage/>} />
+          </Routes>
+        </Router>
+    
+    </AuthProvider>
   );
-}
+};
 
 export default App;
